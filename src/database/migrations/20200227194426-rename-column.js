d5f969f47fb4e6c27690463d57fae178b2cad684
Douglas Cardoso <douglasgodoy1998@hotmail.com>
2020-02-27 18:52:19 -0300

    commit inicial

---
+++ src/database/migrations/20200227194426-rename-column.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.renameColumn('addresses', 'update_at', 'updated_at');
+//   },
+
+  // down: (queryInterface, Sequelize) => {
+/*
+  Add reverting commands here.
+  Return a promise to correctly handle asynchronicity.
+
+  Example:
+  return queryInterface.dropTable('users');
+*/
+//   }
+// };
